f3960f28c557587939de63ed778a9f6bb244fbfc
Gabriel <gwolf@student.42vienna.com>
2023-01-26 14:43:42 +0100

    feat: start of input reading

--- src/main.c
+++ src/main.c
@@ -6,28 +6,16 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:54:09 by gwolf             #+#    #+#             */
-/*   Updated: 2023/01/26 11:30:09 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/01/26 14:06:30 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	t_vars vars;
-	t_data img;
-
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 600, 500, "Hello World!");
-	img.img = mlx_new_image(vars.mlx, 600, 500);
-	img.addr = mlx_get_data_addr(img.img, &img.bit_per_pixel, &img.line_length, &img.endian);
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_mouse_hook(vars.win, mouse_hook, &vars);
-	draw_square(&img, 100, 100, 130, create_trgb(0, 0, 255, 0));
-	draw_triangle(&img, 0, 0, 130);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_hook(vars.win, 2, 1L<<0, close_window, &vars);
-	mlx_hook(vars.win, 6, 1L<<6, mouse_move, &vars);
-	mlx_loop(vars.mlx);
+	if (argc != 2)
+		return (write(2, "ERROR\n", 6));
+	(void)argv;
+	testwindow();
 }
-
