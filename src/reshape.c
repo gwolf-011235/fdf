367eba6288109155bc05ec6dae9c04f20ad5ece7
Gabriel <gwolf@student.42vienna.com>
2023-02-03 11:36:27 +0100

    feat: new redraw ft and shifted around stuff in testwin

--- src/reshape.c
+++ src/reshape.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 10:22:06 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/03 10:39:01 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/03 11:32:19 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,23 @@ void	ft_shape_map(t_map *map)
 	i = 0;
 	while (i < map->sum_points)
 	{
-		map->morph[i].color = gradient(COLOR_TOP, COLOR_MID, map->top, map->morph[i].z);
-		rotate(&map->points[i], map->ang_x, map->ang_y, map->ang_z);
+		map->morph[i].color = gradient(map->color_mid, map->color_top, map->top, map->morph[i].z);
+		rotate(&map->morph[i], map->ang_x, map->ang_y, map->ang_z);
 		map->morph[i].x = map->morph[i].x * map->scale;
 		map->morph[i].y = map->morph[i].y * map->scale;
 		map->morph[i].z = map->morph[i].z * map->scale;
-		i++;
-	}
-	i = 0;
-	while (i < map->sum_points)
-	{
 		map->morph[i] = project_2d(map->morph[i]);
 		map->morph[i].x = map->morph[i].x + map->offset_x;
 		map->morph[i].y = map->morph[i].y + map->offset_y;
 		i++;
 	}
+}
+
+void	ft_redraw(t_data *data)
+{
+		ft_copy_map(&data->map);
+		ft_shape_map(&data->map);
+		fill_background(&data->img);
+		lines(&data->img, &data->map);
+		mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
 }
--- src/testwin.c
+++ src/testwin.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:00:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/02 14:26:19 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/03 11:16:44 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ void testwindow(t_data *data)
 	data->win = mlx_new_window(data->mlx, WIN_X, WIN_Y, "FdF - by gwolf");
 	data->img.img = mlx_new_image(data->mlx, WIN_X, WIN_Y);
 	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bit_per_pixel, &data->img.line_length, &data->img.endian);
-	mlx_key_hook(data->win, key_hook, NULL);
-	mlx_mouse_hook(data->win, mouse_hook, NULL);
-	draw_points(&data->img, &data->map);
 	lines(&data->img, &data->map);
 	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
+	mlx_key_hook(data->win, key_hook, data);
+	mlx_mouse_hook(data->win, mouse_hook, data);
 	mlx_hook(data->win, 2, 1L<<0, close_window, data);
 	mlx_hook(data->win, 6, 1L<<6, mouse_move, data);
 	mlx_loop(data->mlx);
