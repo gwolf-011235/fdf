45cb16514c17fd367192bd28b6e482f622dda2cd
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-02-08 19:15:47 +0100

    feat: prep for boxing mlx fts and stuff

--- src/window.c
+++ src/window.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   testwin.c                                          :+:      :+:    :+:   */
+/*   window.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:00:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/04 22:13:42 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/08 19:11:13 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void testwindow(t_data *data)
+void	ft_init_window(t_data *data)
 {
 	int width;
 	int height;
@@ -20,9 +20,14 @@ void testwindow(t_data *data)
 	data->mlx = mlx_init();
 	mlx_get_screen_size(data->mlx, &width, &height);
 	printf("Screen size: %dx%d\n", width, height);
-	data->win = mlx_new_window(data->mlx, WIN_X, WIN_Y, "FdF - by gwolf");
 	data->img.img = mlx_new_image(data->mlx, WIN_X, WIN_Y);
 	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bit_per_pixel, &data->img.line_length, &data->img.endian);
+	data->win = mlx_new_window(data->mlx, width, height, "FdF - by gwolf");
+}
+
+void testwindow(t_data *data)
+{
+	ft_init_window(data);
 	lines(&data->img, &data->map);
 	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
 	mlx_key_hook(data->win, key_hook, data);
