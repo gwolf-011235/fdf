a52cc9b1afa127e12d9b4820a028d1ca836686c9
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-02-11 08:41:18 +0100

    fix: changed signs from rotations and swapped arpund some lines for better understanding

--- src/rotation.c
+++ src/rotation.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 13:26:37 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/10 17:19:03 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/11 08:39:35 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ void	ft_rotate_x(t_mat4 trans, float roll)
 
 	roll_sin = sin(roll * RAD);
 	roll_cos = cos(roll * RAD);
+	ft_init_mat4(temp);
 	ft_init_mat4(rot_x);
 	rot_x[0][0] = 1;
-	rot_x[3][3] = 1;
-	ft_init_mat4(temp);
 	rot_x[1][1] = roll_cos;
-	rot_x[1][2] = -roll_sin;
-	rot_x[2][1] = roll_sin;
+	rot_x[1][2] = roll_sin;
+	rot_x[2][1] = -roll_sin;
 	rot_x[2][2] = roll_cos;
+	rot_x[3][3] = 1;
 	ft_mult_mat4(rot_x, trans, temp);
 	ft_copy_mat4(temp, trans);
 }
@@ -42,14 +42,14 @@ void	ft_rotate_y(t_mat4 trans, float pitch)
 
 	pitch_sin = sin(pitch * RAD);
 	pitch_cos = cos(pitch * RAD);
+	ft_init_mat4(temp);
 	ft_init_mat4(rot_y);
 	rot_y[1][1] = 1;
-	rot_y[3][3] = 1;
-	ft_init_mat4(temp);
 	rot_y[0][0] = pitch_cos;
-	rot_y[0][2] = pitch_sin;
-	rot_y[2][0] = -pitch_sin;
+	rot_y[0][2] = -pitch_sin;
+	rot_y[2][0] = pitch_sin;
 	rot_y[2][2] = pitch_cos;
+	rot_y[3][3] = 1;
 	ft_mult_mat4(rot_y, trans, temp);
 	ft_copy_mat4(temp, trans);
 }
@@ -63,14 +63,14 @@ void	ft_rotate_z(t_mat4 trans, float yaw)
 
 	yaw_sin = sin(yaw * RAD);
 	yaw_cos = cos(yaw * RAD);
+	ft_init_mat4(temp);
 	ft_init_mat4(rot_z);
 	rot_z[2][2] = 1;
-	rot_z[3][3] = 1;
-	ft_init_mat4(temp);
 	rot_z[0][0] = yaw_cos;
-	rot_z[0][1] = -yaw_sin;
-	rot_z[1][0] = yaw_sin;
+	rot_z[0][1] = yaw_sin;
+	rot_z[1][0] = -yaw_sin;
 	rot_z[1][1] = yaw_cos;
+	rot_z[3][3] = 1;
 	ft_mult_mat4(rot_z, trans, temp);
 	ft_copy_mat4(temp, trans);
 }
