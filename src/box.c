11adabcb1176d63157d3b9de57884cf3ccf36658
Gabriel <gwolf@student.42vienna.com>
2023-02-15 14:44:45 +0100

    feat: new ft fit box

--- src/box.c
+++ src/box.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 20:28:13 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/12 13:57:36 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/15 14:42:50 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,31 @@ void	ft_calc_edges(t_map *map)
 	}
 }
 
+float	ft_fit_box(t_vec3f *init, int size[2], int offset[2])
+{
+	float	scale;
+	int		i;
+	t_vec3f	temp;
+
+	scale = 1;
+	while (1)
+	{
+		i = 0;
+		while (i < 8)
+		{
+			temp.x = (init[i].x * scale + offset[X]) * 1.2;
+			temp.y = (init[i].y * scale + offset[Y]) * 1.2;
+			if (temp.x > size[X] || temp.y > size[Y])
+			{
+				printf("Scale is: %f\n", scale);
+				return (scale);
+			}
+			i++;
+		}
+		scale += 0.1;
+	}
+}
+
 void	ft_draw_box(t_data *data)
 {
 	t_vec3f	*edges;
@@ -48,11 +73,7 @@ void	ft_draw_box(t_data *data)
 	i = 0;
 	while (i < 8)
 	{
-		printf("INIT\n");
-		ft_print_point(edges[i - P]);
 		edges[i] = ft_mult_vec3f_mat4(edges[i - P], data->map.trans);
-		printf("MORPH\n");
-		ft_print_point(edges[i]);
 		i++;
 	}
 	i = 0;
@@ -62,18 +83,18 @@ void	ft_draw_box(t_data *data)
 		colors[1] = BLUE;
 		start = ft_convert_3to2(edges[i]);
 		end = ft_convert_3to2(edges[((i) + 1) % 4]);
-		if (!ft_is_outside(edges[i], data->render.win_x, data->render.win_y) && !ft_is_outside(edges[((i) + 1) % 4], data->render.win_x, data->render.win_y))
+		if (!ft_is_outside(edges[i], data->render.size[X], data->render.size[Y]) && !ft_is_outside(edges[((i) + 1) % 4], data->render.size[X], data->render.size[Y]))
 			draw_line(&data->render, start, end, colors);
 		colors[0] = RED;
 		colors[1] = RED;
 		end = ft_convert_3to2(edges[i + 4]);
-		if (!ft_is_outside(edges[i], data->render.win_x, data->render.win_y) && !ft_is_outside(edges[i + 4], data->render.win_x, data->render.win_y))
+		if (!ft_is_outside(edges[i], data->render.size[X], data->render.size[Y]) && !ft_is_outside(edges[i + 4], data->render.size[X], data->render.size[Y]))
 			draw_line(&data->render, start, end, colors);
 		colors[0] = GREEN;
 		colors[1] = GREEN;
 		start = ft_convert_3to2(edges[i + 4]);
 		end = ft_convert_3to2(edges[((i + 1) % 4) + 4]);
-		if (!ft_is_outside(edges[i + 4], data->render.win_x, data->render.win_y) && !ft_is_outside(edges[((i + 1) % 4) + 4], data->render.win_x, data->render.win_y))
+		if (!ft_is_outside(edges[i + 4], data->render.size[X], data->render.size[Y]) && !ft_is_outside(edges[((i + 1) % 4) + 4], data->render.size[X], data->render.size[Y]))
 			draw_line(&data->render, start, end, colors);
 		i++;
 	}
