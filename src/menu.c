3b32b93bbf62e47f91a9aef2e1b203ee69f6cf4c
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-02-15 08:07:33 +0100

    feat: test of font set function

--- src/menu.c
+++ src/menu.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 10:38:34 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/09 15:16:33 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/13 07:17:11 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ void	ft_init_menu(t_data *data)
 	int j;
 
 	data->menu.ptr = mlx_xpm_file_to_image(data->mlx, "image/menu.xpm", &i, &j);
-	mlx_put_image_to_window(data->mlx, data->win, data->menu.ptr, 0, 0);
+	mlx_set_font(data->mlx, data->win, "-misc-fixed-medium-r-normal--7-50-100-100-c-50-iso8859-1");
+	mlx_string_put(data->mlx, data->win, 50, 50, 0x00FFFFFF, "Hello, World!");
+	//mlx_put_image_to_window(data->mlx, data->win, data->menu.ptr, 0, 0);
 }
