aaa87ec9379fbfefe531237cc8d221b7fa61f387
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-02-17 10:22:22 +0100

    feat: implemented pixel array

--- src/box.c
+++ src/box.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 20:28:13 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/16 21:24:52 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/17 09:27:07 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ void	ft_calc_edges(t_map *map)
 	while (i < 8)
 	{
 		if (i == 1 || i == 2 || i == 5 || i == 6)
-			map->edges[i].x = map->max[X];
+			map->edges[i + i].x = map->max[X];
 		else
-			map->edges[i].x = map->min[X];
+			map->edges[i + i].x = map->min[X];
 		if (i == 2 || i == 3 || i == 6 || i == 7)
-			map->edges[i].y = map->max[Y];
+			map->edges[i + i].y = map->max[Y];
 		else
-			map->edges[i].y = map->min[Y];
+			map->edges[i + i].y = map->min[Y];
 		if (i < 4)
-			map->edges[i].z = map->min[Z];
+			map->edges[i + i].z = map->min[Z];
 		else
-			map->edges[i].z = map->max[Z];
+			map->edges[i + i].z = map->max[Z];
 		i++;
 	}
 }
@@ -47,10 +47,11 @@ float	ft_fit_box(t_vec3f *init, int size[2], int offset[2])
 		i = 0;
 		while (i < 8)
 		{
-			temp.x = (init[i].x * scale + offset[X]) * 1.2;
-			temp.y = (init[i].y * scale + offset[Y]) * 1.2;
+			temp.x = (init[i + i].x * scale + offset[X]) * 1.2;
+			temp.y = (init[i + i].y * scale + offset[Y]) * 1.2;
 			if (temp.x > size[X] || temp.y > size[Y])
 			{
+				scale = floor(scale);
 				printf("Scale is: %f\n", scale);
 				return (scale);
 			}
@@ -62,40 +63,32 @@ float	ft_fit_box(t_vec3f *init, int size[2], int offset[2])
 
 void	ft_draw_box(t_data *data)
 {
-	t_vec3f	*edges;
+	t_point	*edges;
 	t_point	start;
 	t_point	end;
 	int	i;
-	int colors[2];
 
-
-	edges = data->map.edges P;
-	i = 0;
-	while (i < 8)
-	{
-		edges[i] = ft_mult_vec3f_mat4(edges[i - P], data->map.trans);
-		i++;
-	}
+	edges = data->map.pixel + data->map.sum_points;
 	i = 0;
 	while (i < 4)
 	{
-		colors[0] = BLUE;
-		colors[1] = BLUE;
-		start = ft_convert_3to2(edges[i]);
-		end = ft_convert_3to2(edges[((i) + 1) % 4]);
-		if (!ft_is_outside(edges[i], data->render.size[X], data->render.size[Y]) && !ft_is_outside(edges[((i) + 1) % 4], data->render.size[X], data->render.size[Y]))
-			draw_line(&data->render, start, end, colors);
-		colors[0] = RED;
-		colors[1] = RED;
-		end = ft_convert_3to2(edges[i + 4]);
-		if (!ft_is_outside(edges[i], data->render.size[X], data->render.size[Y]) && !ft_is_outside(edges[i + 4], data->render.size[X], data->render.size[Y]))
-			draw_line(&data->render, start, end, colors);
-		colors[0] = GREEN;
-		colors[1] = GREEN;
-		start = ft_convert_3to2(edges[i + 4]);
-		end = ft_convert_3to2(edges[((i + 1) % 4) + 4]);
-		if (!ft_is_outside(edges[i + 4], data->render.size[X], data->render.size[Y]) && !ft_is_outside(edges[((i + 1) % 4) + 4], data->render.size[X], data->render.size[Y]))
-			draw_line(&data->render, start, end, colors);
+		start = edges[i];
+		end = edges[(i + 1) % 4];
+		start.color = BLUE;
+		end.color = BLUE;
+		if (ft_is_inside(start, data->render.size[X], data->render.size[Y]) && ft_is_inside(end, data->render.size[X], data->render.size[Y]))
+			draw_line(&data->render, start, end);
+		end = edges[i + 4];
+		start.color = RED;
+		end.color = RED;
+		if (ft_is_inside(start, data->render.size[X], data->render.size[Y]) && ft_is_inside(end, data->render.size[X], data->render.size[Y]))
+			draw_line(&data->render, start, end);
+		start = edges[i + 4];
+		end = edges[((i + 1) % 4) + 4];
+		start.color = GREEN;
+		end.color = GREEN;
+		if (ft_is_inside(start, data->render.size[X], data->render.size[Y]) && ft_is_inside(end, data->render.size[X], data->render.size[Y]))
+			draw_line(&data->render, start, end);
 		i++;
 	}
 }
