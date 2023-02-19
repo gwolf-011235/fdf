060926b562f611d4f4a248c312727b575007801e
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-02-19 07:17:35 +0100

    fix: change to accomodate new ft

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:54:09 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/17 09:54:32 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/18 22:59:51 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,7 @@ int	main(int argc, char *argv[])
 	ft_check_map(&data.map, argv[1]);
 	ft_parse_map(&data.map);
 	ft_init_window(&data);
-	map->scale = ft_fit_box(map->edges, data.render.size, map->offset);
-	ft_init_project(&data.map);
+	ft_init_project(map);
 	//test(&data);
 	//ft_shape_map(&data.map);
 	testwindow(&data);
