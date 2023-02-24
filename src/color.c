170e65fb0f8163d6872e3cebbaee49f76f7a5a7b
Gabriel <gwolf@student.42vienna.com>
2023-02-24 16:55:07 +0100

    fix: changed gradient function to also include alpha, removed create_trgb

--- src/color.c
+++ src/color.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:29:34 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/23 15:38:24 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/02/24 15:05:58 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,28 @@ int	ft_alpha_blend(int new_color, int old_color)
 	return ((start[0] << 24) | (blend[0] << 16) | (blend[1] << 8) | blend[2]);
 }
 
-int	gradient(int color_start, int color_end, double len, double pos)
+int	gradient(int start, int end, double len, double pos)
 {
-	int	factor[3];
-	int	new_col[3];
+	int		factor[4];
+	int		n_col[4];
+	double	invert_len;
 
-	factor[0] = ((color_end >> 16 & 0xFF) - (color_start >> 16 & 0xFF)) / len;
-	factor[1] = ((color_end >> 8 & 0xFF) - (color_start >> 8 & 0xFF)) / len;
-	factor[2] = ((color_end & 0xFF) - (color_start & 0xFF)) / len;
-	new_col[0] = factor[0] * pos + (color_start >> 16 & 0xFF);
-	new_col[1] = factor[1] * pos + (color_start >> 8 & 0xFF);
-	new_col[2] = factor[2] * pos + (color_start & 0xFF);
-	return (create_trgb(0, new_col[0], new_col[1], new_col[2]));
+	invert_len = 1.0 / len;
+	factor[0] = ((end >> 24 & 0xFF) - (start >> 24 & 0xFF)) * invert_len;
+	factor[1] = ((end >> 16 & 0xFF) - (start >> 16 & 0xFF)) * invert_len;
+	factor[2] = ((end >> 8 & 0xFF) - (start >> 8 & 0xFF)) * invert_len;
+	factor[3] = ((end & 0xFF) - (start & 0xFF)) * invert_len;
+	n_col[0] = factor[0] * pos + (start >> 24 & 0xFF);
+	n_col[1] = factor[1] * pos + (start >> 16 & 0xFF);
+	n_col[2] = factor[2] * pos + (start >> 8 & 0xFF);
+	n_col[3] = factor[3] * pos + (start & 0xFF);
+	return ((n_col[0] << 24) | (n_col[1] << 16) | (n_col[2] << 8) | n_col[3]);
 }
 
 void	ft_set_pattern(int pattern[3], int choice)
 {
 	const int colorschemes[3][3] = {
-		{RED, YELLOW, BLUE},
+		{YELLOW, RED, BLUE},
 		{PURPLE, GREEN, ORANGE},
 		{LIGHTBLUE, PINK, LIME}
 	};
