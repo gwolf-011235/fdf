602ac54ca38428d5767029a6ae3ed3d336492561
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-04 21:56:53 +0100

    fix: prototype for init

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:54:09 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/01 08:39:19 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/03 23:22:13 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 int	main(int argc, char *argv[])
 {
 	static t_data	data;
-	t_map			*map;
 
-	map = &data.map;
 	if (argc != 2)
 		ft_terminate(ERR_ARGS);
 	ft_map_init(&data.map);
 	ft_check_map(&data.map, argv[1]);
 	ft_parse_map(&data.map);
 	ft_init_window(&data);
+	ft_init_project(&data);
 	//test(&data);
-	ft_init_project(map, data.render.size);
 	testwindow(&data);
 }
