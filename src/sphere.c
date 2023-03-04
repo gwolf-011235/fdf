e7aeaf86b6242cfb34090e9d2b666c96a69d6dcd
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-04 22:04:09 +0100

    fix: adjust ft to fit into new malloc scheme

--- src/sphere.c
+++ src/sphere.c
@@ -6,46 +6,41 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 07:34:37 by gwolf             #+#    #+#             */
-/*   Updated: 2023/02/28 19:54:20 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/04 21:39:37 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	ft_convert_spherical(t_map *map, int sum)
+void	ft_set_ang_coords(t_map *map, int sum)
 {
 	int		i;
-	float	step_longi;
-	float	step_lati;
+	float	step_lon;
+	float	step_lat;
 
 	i = 0;
-	step_longi = (M_PI * 2) / map->max[X];
-	step_lati = M_PI / map->max[Y];
+	step_lon = (2 * M_PI) / map->max[X];
+	step_lat = M_PI / map->max[Y];
 	map->radius = map->width / (2 * M_PI);
 	while (i < sum)
 	{
-		map->points[i + i].angle[LON] = -(map->points[i + i].x) * step_longi;
-		map->points[i + i].angle[LAT] = map->points[i + i].y * step_lati;
-		if (map->points[i + i].y > 0)
-			map->points[i + i].angle[LAT] = (map->points[i + i].y + (map->max[Y] / 2)) * step_lati - 0.5 * step_lati;
-		else
-			map->points[i + i].angle[LAT] = (map->points[i + i].y + (map->max[Y] / 2) - 1) * step_lati + 0.5 * step_lati;
+		map->ang_coord[i].lon = map->points[i].x * step_lon;
+		map->ang_coord[i].lat = map->points[i].y * step_lat;
 		i++;
 	}
 }
 
-void	ft_convert_sphere2cart(t_map *map, t_vec3f *polar)
+void	ft_calc_sphere_points(t_map *map, t_coord *ang_coord, t_vec3f *polar)
 {
 	int	i;
 
 	i = 0;
-	float factor = (M_PI * 2) / map->max[Z];
 	while (i < map->sum_points)
 	{
-		polar[i + i].x = (map->radius + factor * map->points[i + i].z) * sin(map->points[i + i].angle[LAT]) * cos(map->points[i + i].angle[LON]);
-		polar[i + i].y = (map->radius + factor * map->points[i + i].z) * sin(map->points[i + i].angle[LAT]) * sin(map->points[i + i].angle[LON]);
-		polar[i + i].z = (map->radius + factor * map->points[i + i].z) * cos(map->points[i + i].angle[LAT]);
-		polar[i + i].color = map->points[i + i].color;
+		polar[i].x = (map->radius + map->points[i].z) * sin(ang_coord[i].lat) * cos(ang_coord[i].lon);
+		polar[i].y = (map->radius + map->points[i].z) * sin(ang_coord[i].lat) * sin(ang_coord[i].lon);
+		polar[i].z = (map->radius + map->points[i].z) * cos(ang_coord[i].lat);
+		polar[i].color = map->points[i].color;
 		i++;
 	}
 }
