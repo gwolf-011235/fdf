b1bee7008d0c2056ae3dc8c045db6df8c20473ca
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-04 21:57:45 +0100

    fix: added some new initial values - need to be made flexible

--- src/map_utils.c
+++ src/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 09:27:53 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/01 15:36:50 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/04 21:48:12 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 void	ft_map_init(t_map *map)
 {
 	ft_set_pattern(map->pattern, 0, 5);
-	map->props.angle[X] = 45;
-	map->props.angle[Z] = atan(sqrt(2));
+	map->props.angle[X] = atan(sqrt(2)) * 180 / M_PI;
+	map->props.angle[Y] = 0;
+	map->props.angle[Z] = 45;
+	map->props.translate[Z] = 1;
 	map->factor = 1;
+	map->props.sphere = true;
 }
 
 void	ft_find_extremes(t_map *map, int z)
