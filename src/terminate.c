d303eba8ad642b3d61a7d985ad7718d629c5ffb7
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-04 22:04:34 +0100

    fix: added free for ang_coord malloc

--- src/terminate.c
+++ src/terminate.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 14:46:48 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/04 10:11:37 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/04 22:02:11 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ void	ft_free_map_ptr(t_map *map, char *string)
 		free(map->points);
 	if (map->z_storage)
 		free(map->z_storage);
+	if (map->ang_coord)
+		free(map->ang_coord);
 	ft_terminate(string);
 }
 
@@ -52,6 +54,7 @@ void	ft_free_mlx(t_data *data, char *string, bool error)
 {
 	free(data->map.points);
 	free(data->map.z_storage);
+	free(data->map.ang_coord);
 	if (data->render.ptr)
 		mlx_destroy_image(data->mlx, data->render.ptr);
 	if (data->menu.ptr)
