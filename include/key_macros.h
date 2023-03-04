900098b6317fc6559b3b925405f11eb9a3a919b9
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-04 20:22:36 +0100

    feat: added some key macro

--- include/key_macros.h
+++ include/key_macros.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 10:32:42 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/01 11:50:39 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/04 19:44:27 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@
 # define KEY_ARROW_RIGHT 65363
 # define KEY_ARROW_DOWN 65364
 # define KEY_A 97
-# define KEY_S 115
 # define KEY_D 100
+# define KEY_E 101
 # define KEY_F 102
 # define KEY_G 103
 # define KEY_H 104
 # define KEY_Q 113
+# define KEY_R 114
+# define KEY_S 115
 # define KEY_W 119
 # define KEY_ONE 49
 # define KEY_TWO 50
