678c62a389c7b6228fc00c64828901f431c8e2aa
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-08 18:07:30 +0100

    fix: small changes

--- src/menu.c
+++ src/menu.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 10:38:34 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/05 21:46:49 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/06 22:27:57 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_init_menu(t_data *data)
 
 	data->menu.ptr = mlx_xpm_file_to_image(data->mlx, "image/menu.xpm", &i, &j);
 	mlx_set_font(data->mlx, data->win, 
-			"-misc-fixed-*-*-*-*-*-200-*-*-*-*-iso8859-1");
-	mlx_string_put(data->mlx, data->win, 50, 50, 0x00FFFFFF, "Hello, World!");
-	//mlx_put_image_to_window(data->mlx, data->win, data->menu.ptr, 0, 0);
+			"-misc-fixed-medium-r-normal-*-20-*-*-100-*-*-iso8859-1");
+	mlx_put_image_to_window(data->mlx, data->win, data->menu.ptr, 0, 0);
+	mlx_string_put(data->mlx, data->win, 120, 150, 0x0, "Hello, World!");
 }
