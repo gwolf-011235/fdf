560db8f335ccb7419b8076c1cc422bbb30943b08
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-12 18:55:29 +0100

    feat: render is now ptr[2] to t_img, so that it can be swapped. Is malloced > should be free'd

--- src/window.c
+++ src/window.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:00:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/12 08:33:57 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/12 18:38:57 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,28 @@
 
 void	ft_init_window(t_data *data)
 {
-	int	win_x;
-	int	win_y;
+	int	*screen;
 
+	screen = data->screen;
 	data->mlx = mlx_init();
 	if (!data->mlx)
 		ft_free_mlx(data, ERR_MLX, true);
-	mlx_get_screen_size(data->mlx, &win_x, &win_y);
-	ft_printf("🖥️  Screen size\n   |%d x %d|\n\n", win_x, win_y);
-	data->win = mlx_new_window(data->mlx, win_x, win_y, "FdF - by gwolf");
+	mlx_get_screen_size(data->mlx, &data->screen[X], &data->screen[Y]);
+	ft_printf("🖥️  Screen size\n   |%d x %d|\n\n", data->screen[X], data->screen[Y]);
+	data->win = mlx_new_window(data->mlx, screen[X], screen[Y], "FdF - by gwolf");
 	if (!data->win)
 		ft_free_mlx(data, ERR_WIN, true);
-	ft_init_image(data, &data->render, win_x, win_y);
-	ft_init_image(data, &data->menu, (win_x / 6), win_y);
-	data->map.props.canvas[X] = win_x;
-	data->map.props.canvas[Y] = win_y;
-	data->map.props.canvas[OFFSET_X] = win_x / 2;
-	data->map.props.canvas[OFFSET_Y] = win_y / 2;
+	data->render[0] = malloc(sizeof(t_img));
+	data->render[1] = malloc(sizeof(t_img));
+	ft_init_image(data, data->render[0], screen[X], screen[Y]);
+	ft_init_image(data, data->render[1], screen[X] - MENU_WIDTH, screen[Y]);
+	printf("0. line_len: %d\n", data->render[0]->line_len);
+	printf("1. line_len: %d\n", data->render[1]->line_len);
+	ft_init_image(data, &data->menu, MENU_WIDTH, screen[Y]);
+	data->map.props.canvas[X] = screen[X];
+	data->map.props.canvas[Y] = screen[Y];
+	data->map.props.canvas[OFFSET_X] = screen[X] / 2;
+	data->map.props.canvas[OFFSET_Y] = screen[Y] / 2;
 }
 
 void	ft_init_image(t_data *data, t_img *img, int win_x, int win_y)
