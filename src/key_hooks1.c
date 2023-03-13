22ba763e2f8b4336d98f58c5ec51c02219ac4f01
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-13 08:19:51 +0100

    feat: added new key hook press2 to split function - norminette

--- src/key_hooks1.c
+++ src/key_hooks1.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:16:59 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/12 17:34:11 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/13 08:16:47 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,14 @@
 
 int	ft_key_hook_press(int key, t_data *data)
 {
-	if (key >= KEY_ARROW_LEFT && key <= KEY_ARROW_DOWN)
-		ft_key_translate(key, &data->map);
-	else if (key == KEY_W || key == KEY_S || key == KEY_A || key == KEY_D)
-		ft_key_angle(key, &data->map);
-	else if (key == KEY_Q || key == KEY_E)
-		ft_key_angle2(key, &data->map);
-	else if (key == KEY_R || key == KEY_F)
-		ft_key_zoom(key, &data->map);
-	else if (key == KEY_X || key == KEY_C)
-		ft_key_scale(key, &data->map);
-	else if (key == KEY_ONE || key == KEY_TWO || key == KEY_THREE
-		|| key == KEY_FOUR || key == KEY_FIVE)
-		ft_key_color(key, &data->map);
-	else if (key == KEY_SPACE)
-		ft_key_sphere(key, &data->map);
-	else if (key == KEY_SIX || key == KEY_SEVEN || key == KEY_EIGHT
+	if ((key >= KEY_ARROW_LEFT && key <= KEY_ARROW_DOWN) || key == KEY_W 
+		|| key == KEY_S || key == KEY_A || key == KEY_D || key == KEY_Q
+		|| key == KEY_E || key == KEY_R || key == KEY_F || key == KEY_X
+		|| key == KEY_C || key == KEY_ONE || key == KEY_TWO || key == KEY_THREE
+		|| key == KEY_FOUR || key == KEY_FIVE || key == KEY_SPACE
+		|| key == KEY_SIX || key == KEY_SEVEN || key == KEY_EIGHT
 		|| key == KEY_NINE)
-		ft_key_view(key, &data->map);
+		ft_key_hook_press2(key, &data->map);
 	else if (key == KEY_B || key == KEY_N || key == KEY_M
 		|| key == KEY_G || key == KEY_ESC)
 		ft_key_stuff(key, &data->map, data);
@@ -41,9 +31,32 @@ int	ft_key_hook_press(int key, t_data *data)
 	else if (key == KEY_K || key == KEY_L)
 		ft_key_skip(key, &data->map);
 	else if (key == KEY_I)
-		ft_key_menu(data);
-	printf("Key: %d", key);
+		ft_key_toggle_menu(data);
+	else if (key == KEY_H)
+		ft_key_toggle_controls(data);
 	return (0);
+}
+
+void	ft_key_hook_press2(int key, t_map *map)
+{
+	if (key >= KEY_ARROW_LEFT && key <= KEY_ARROW_DOWN)
+		ft_key_translate(key, map);
+	else if (key == KEY_W || key == KEY_S || key == KEY_A || key == KEY_D)
+		ft_key_angle(key, map);
+	else if (key == KEY_Q || key == KEY_E)
+		ft_key_angle2(key, map);
+	else if (key == KEY_R || key == KEY_F)
+		ft_key_zoom(key, map);
+	else if (key == KEY_X || key == KEY_C)
+		ft_key_scale(key, map);
+	else if (key == KEY_ONE || key == KEY_TWO || key == KEY_THREE
+		|| key == KEY_FOUR || key == KEY_FIVE)
+		ft_key_color(key, map);
+	else if (key == KEY_SPACE)
+		ft_key_sphere(key, map);
+	else if (key == KEY_SIX || key == KEY_SEVEN || key == KEY_EIGHT
+		|| key == KEY_NINE)
+		ft_key_view(key, map);
 }
 
 
