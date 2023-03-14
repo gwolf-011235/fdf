6fb8757895539d228d046be55855c30a40dc5763
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-14 18:20:47 +0100

    test for nbr puts

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 22:15:46 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/09 19:14:33 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/14 01:39:08 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	test(t_data *data)
 {
-	ft_init_menu(data);
+	float	num = 3.145;
+	int		num2 = 3650;
+	char	str[20];
+
+	(void)data;
+	ft_ftoa_in_place(num, str, 5);
+	printf("This is str: %s\n", str);
+	ft_memset(str, 0, 20);
+	ft_itoa_in_place(num2, str);
+	printf("This is str2: %s\n", str);
 }
