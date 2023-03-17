a8a52a0949da35dce9676ca5c053003c2d9bd4ab
Gabriel <gwolf@student.42vienna.com>
2023-03-17 15:04:01 +0100

    fix: menu is now showing in the right places

--- src/menu.c
+++ src/menu.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 10:38:34 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/16 02:01:25 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/17 15:01:28 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,21 +96,25 @@ void	ft_write_menu(t_data *data)
 	ft_mlx_put_int(data, (t_pos){130, 145}, data->map.height, 3);
 	mlx_string_put(data->mlx, data->win, 30, 165, 0x0, "> Points:");
 	ft_mlx_put_int(data, (t_pos){130, 165}, data->map.sum_points, 3);
-	mlx_string_put(data->mlx, data->win, 30, 205, 0x0, "Hex-Colors:");
+	mlx_string_put(data->mlx, data->win, 30, 185, 0x0, "Hex-Colors:");
 	if (data->map.hex)
-		mlx_string_put(data->mlx, data->win, 150, 205, 0x00FF00, "found");
+		mlx_string_put(data->mlx, data->win, 150, 185, 0x00FF00, "found");
 	else
-		mlx_string_put(data->mlx, data->win, 150, 205, 0x0, "no");
-	mlx_string_put(data->mlx, data->win, 110, 315, 0x0, "yaw   (x):");
-	mlx_string_put(data->mlx, data->win, 110, 340, 0x0, "pitch (y):");
-	mlx_string_put(data->mlx, data->win, 110, 365, 0x0, "roll  (z):");
-	mlx_string_put(data->mlx, data->win, 110, 430, 0x0, "zoom:");
-	mlx_string_put(data->mlx, data->win, 110, 510, 0x0, "z-scale:");
-	mlx_string_put(data->mlx, data->win, 110, 590, 0x0, "mod-level:");
-	mlx_string_put(data->mlx, data->win, 110, 655, 0x0, "          x:");
-	mlx_string_put(data->mlx, data->win, 110, 670, 0x0, "translate");
-	mlx_string_put(data->mlx, data->win, 110, 685, 0x0, "          y:");
-	mlx_string_put(data->mlx, data->win, 110, 770, 0x0, "sampling:");
+		mlx_string_put(data->mlx, data->win, 150, 185, 0x0, "no");
+	mlx_string_put(data->mlx, data->win, 100, 285, 0x0, "yaw   (x):");
+	mlx_string_put(data->mlx, data->win, 100, 310, 0x0, "pitch (y):");
+	mlx_string_put(data->mlx, data->win, 100, 335, 0x0, "roll  (z):");
+	mlx_string_put(data->mlx, data->win, 100, 390, 0x0, "zoom:");
+	mlx_string_put(data->mlx, data->win, 100, 465, 0x0, "z-scale:");
+	mlx_string_put(data->mlx, data->win, 100, 535, 0x0, "mod-level:");
+	mlx_string_put(data->mlx, data->win, 100, 595, 0x0, "          x:");
+	mlx_string_put(data->mlx, data->win, 100, 605, 0x0, "translate");
+	mlx_string_put(data->mlx, data->win, 100, 620, 0x0, "          y:");
+	mlx_string_put(data->mlx, data->win, 100, 680, 0x0, "sampling:");
+	mlx_string_put(data->mlx, data->win, 100, 750, 0x0, "design:");
+	mlx_string_put(data->mlx, data->win, 100, 815, 0x0, "view:");
+	mlx_string_put(data->mlx, data->win, 100, 865, 0x0, "FPS:");
+	mlx_string_put(data->mlx, data->win, 100, 890, 0x0, "Target:");
 }
 
 void	ft_mlx_put_viewname(t_data *data, uint8_t index, int pos[2])
@@ -137,34 +141,45 @@ void	ft_mlx_put_colorscheme(t_data *data, uint8_t index, int pos[2])
 
 void	ft_update_menu(t_data *data)
 {
-	mlx_put_image_to_window(data->mlx, data->win, data->menu[2]->ptr, 220, 280);
-	ft_mlx_put_int(data, (t_pos){250, 315}, data->map.props.angle[X], 3);
-	ft_mlx_put_int(data, (t_pos){250, 340}, data->map.props.angle[Y], 3);
-	ft_mlx_put_int(data, (t_pos){250, 365}, data->map.props.angle[Z], 3);
+	mlx_put_image_to_window(data->mlx, data->win, data->menu[2]->ptr, 210, 260);
+	ft_mlx_put_int(data, (t_pos){230, 285}, data->map.props.angle[X], 3);
+	ft_mlx_put_int(data, (t_pos){230, 310}, data->map.props.angle[Y], 3);
+	ft_mlx_put_int(data, (t_pos){230, 335}, data->map.props.angle[Z], 3);
 	if (data->map.props.scale >= 1000)
-		mlx_string_put(data->mlx, data->win, 240, 430, RED, "MAX");
+		mlx_string_put(data->mlx, data->win, 230, 390, RED, "MAX");
 	else if (data->map.props.scale < 0.1)
-		mlx_string_put(data->mlx, data->win, 240, 430, RED, "MIN");
+		mlx_string_put(data->mlx, data->win, 230, 390, RED, "MIN");
 	else
-		ft_mlx_put_float(data, (t_pos){220, 430}, data->map.props.scale, 2);
+		ft_mlx_put_float(data, (t_pos){210, 390}, data->map.props.scale, 2);
 	if (data->map.props.scale_z >= 10.0)
-		mlx_string_put(data->mlx, data->win, 240, 510, RED, "MAX");
+		mlx_string_put(data->mlx, data->win, 230, 465, RED, "MAX");
 	else if (data->map.props.scale_z <= -10.0)
-		mlx_string_put(data->mlx, data->win, 240, 510, RED, "MIN");
+		mlx_string_put(data->mlx, data->win, 230, 465, RED, "MIN");
 	else
-		ft_mlx_put_float(data, (t_pos){220, 510}, data->map.props.scale_z, 2);
+		ft_mlx_put_float(data, (t_pos){210, 465}, data->map.props.scale_z, 2);
 	if (data->map.factor == 1)
-		mlx_string_put(data->mlx, data->win, 240, 590, GREEN, "min");
+		mlx_string_put(data->mlx, data->win, 230, 535, GREEN, "min");
 	else if (data->map.factor == 3)
-		mlx_string_put(data->mlx, data->win, 240, 590, YELLOW, "Mid");
+		mlx_string_put(data->mlx, data->win, 230, 535, YELLOW, "Mid");
 	else 
-		mlx_string_put(data->mlx, data->win, 240, 590, RED, "MAX");
-	ft_mlx_put_int(data, (t_pos){210, 655}, data->map.props.translate[X], 7);
-	ft_mlx_put_int(data, (t_pos){210, 685}, data->map.props.translate[Y], 7);
-	mlx_string_put(data->mlx, data->win, 245, 100, 0x0, "1:");
-	ft_mlx_put_int(data, (t_pos){250, 100}, data->map.skip, 3);
-	ft_mlx_put_viewname(data, data->map.props.view, (t_pos){220, 120});
-	ft_mlx_put_colorscheme(data, data->map.pattern[4], (t_pos){220, 140});
-	ft_mlx_put_int(data, (t_pos){220, 160}, 1000 / data->fps, 3);
+		mlx_string_put(data->mlx, data->win, 230, 535, RED, "MAX");
+	if (data->map.props.translate[X] >= 5000)
+		mlx_string_put(data->mlx, data->win, 230, 595, RED, "MAX");
+	else if (data->map.props.scale_z <= -5000)
+		mlx_string_put(data->mlx, data->win, 230, 595, RED, "MIN");
+	else
+		ft_mlx_put_int(data, (t_pos){195, 595}, data->map.props.translate[X], 7);
+	if (data->map.props.translate[Y] >= 5000)
+		mlx_string_put(data->mlx, data->win, 230, 620, RED, "MAX");
+	else if (data->map.props.scale_z <= -5000)
+		mlx_string_put(data->mlx, data->win, 230, 620, RED, "MIN");
+	else
+		ft_mlx_put_int(data, (t_pos){195, 620}, data->map.props.translate[Y], 7);
+	mlx_string_put(data->mlx, data->win, 210, 680, 0x0, "1:");
+	ft_mlx_put_int(data, (t_pos){213, 680}, data->map.skip, 3);
+	ft_mlx_put_colorscheme(data, data->map.pattern[4], (t_pos){210, 750});
+	ft_mlx_put_viewname(data, data->map.props.view, (t_pos){210, 815});
+	ft_mlx_put_int(data, (t_pos){210, 865}, 1000 / data->fps, 3);
+	ft_mlx_put_int(data, (t_pos){210, 890}, FPS_TARGET, 3);
 
 }
