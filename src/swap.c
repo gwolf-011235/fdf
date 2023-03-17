16f0334b65d1f53ac539c61cfd0c2ddaff2d9989
Gabriel <gwolf@student.42vienna.com>
2023-03-17 16:43:18 +0100

    fix: norminette

--- src/swap.c
+++ src/swap.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 18:50:56 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/12 18:52:00 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/17 16:43:02 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_swap_img_ptr(t_img **a, t_img **b)
 {
-	t_img *temp;
+	t_img	*temp;
 
 	temp = *a;
 	*a = *b;
