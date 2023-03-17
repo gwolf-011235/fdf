35995f7ef3544da4b5ac262679b4362b1b92d21d
Gabriel <gwolf@student.42vienna.com>
2023-03-17 16:23:53 +0100

    fix: norminette

--- src/wobble.c
+++ src/wobble.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 13:44:20 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/10 15:49:12 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/17 16:23:34 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,14 @@ void	ft_wobble(t_vec3f *morph, t_vec3f *points, t_mat4 mat, t_map *map)
 	{
 		neg = 1;
 		morph[i].x = points[i].x * mat[0][0] + points[i].y * mat[1][0]
-			+ points[i].z * mat[2][0] + mat[3][0] + (rand() % (map->factor * 2)) * neg;
+			+ points[i].z * mat[2][0] + mat[3][0]
+			+ (rand() % (map->factor * 2)) * neg;
 		morph[i].y = points[i].x * mat[0][1] + points[i].y * mat[1][1]
-			+ points[i].z * mat[2][1] + mat[3][1] + (rand() % (map->factor * 2)) * neg;
+			+ points[i].z * mat[2][1] + mat[3][1]
+			+(rand() % (map->factor * 2)) * neg;
 		morph[i].z = points[i].x * mat[0][2] + points[i].y * mat[1][2]
-			+ points[i].z * mat[2][2] + mat[3][2] + (rand() % (map->factor * 2)) * neg;
+			+ points[i].z * mat[2][2] + mat[3][2]
+			+ (rand() % (map->factor * 2)) * neg;
 		if (morph[i].x < 0 || morph[i].x > map->props.canvas[X]
 			|| morph[i].y < 0 || morph[i].y > map->props.canvas[Y])
 			morph[i].hidden = true;
