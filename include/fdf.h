b475f6acae63c8ed04070a8d04a00c44b1e1acf1
Gabriel <gwolf@student.42vienna.com>
2023-03-22 10:59:31 +0100

    norm: fdf.h

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:26:06 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/22 10:14:11 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/22 10:27:44 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@
 # define POP 4
 # define FPS_TARGET 30
 
-typedef	int	t_pos[2];
+typedef int		t_pos[2];
 
 typedef float	t_mat4[4][4];
 
@@ -104,7 +104,7 @@ typedef struct s_pixel {
 typedef struct s_bresvars {
 	t_pixel	start;
 	t_pixel	end;
-	t_pixel delta;
+	t_pixel	delta;
 	int		step[2];
 	int		error[2];
 	int		len;
@@ -170,14 +170,14 @@ typedef struct s_mouse {
 }	t_mouse;
 
 typedef struct s_data {
-	void	*mlx;
-	void	*win;
-	int		screen[2];
-	t_img	*images;
-	t_img	*render[2];
-	t_img	*menu[3];
-	t_map	map;
-	t_mouse	mouse;
+	void		*mlx;
+	void		*win;
+	int			screen[2];
+	t_img		*images;
+	t_img		*render[2];
+	t_img		*menu[3];
+	t_map		map;
+	t_mouse		mouse;
 	t_calc_ft	calc_ft;
 	t_draw_ft	draw_ft;
 	uint64_t	created_at;
