c8c8da44ead672c295c5d903caf8ec7a92eb6745
Gabriel <gwolf@student.42vienna.com>
2023-03-22 11:00:08 +0100

    fix: deleted doubled ft_init_menu

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:54:09 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/22 10:07:37 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/22 10:38:14 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,6 @@ int	main(int argc, char *argv[])
 	ft_set_ft_ptr(&data, 2);
 	srand(time(NULL));
 	ft_init_window(&data);
-	ft_init_menu(&data);
 	ft_init_project(&data);
 	data.created_at = ft_get_timeofday_ms();
 	ft_start_mlx_loop(&data);
