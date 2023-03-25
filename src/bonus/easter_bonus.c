26e49f585eac1fd9e7fe523f583e3cdc6b28692c
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-25 22:30:30 +0100

    fix: removed visibility check because own function

--- src/bonus/easter_bonus.c
+++ src/bonus/easter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 13:44:20 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/23 17:34:32 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/25 22:28:41 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,6 @@ void	ft_wobble(t_vec3f *morph, t_vec3f *points, t_mat4 mat, t_map *map)
 		morph[i].z = points[i].x * mat[0][2] + points[i].y * mat[1][2]
 			+ points[i].z * mat[2][2] + mat[3][2]
 			+ (rand() % (map->factor * 2)) * neg;
-		if (morph[i].x < 0 || morph[i].x > map->props.canvas[X]
-			|| morph[i].y < 0 || morph[i].y > map->props.canvas[Y])
-			morph[i].hidden = true;
-		else
-			morph[i].hidden = false;
 		i++;
 	}
 }
